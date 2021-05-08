df91ee3dc6c2ac0d97305fd204a9173c969c03e9
26797767+krnik <26797767+krnik@users.noreply.github.com>
2021-05-08 13:26:22 +0200

    chore: add test_shared snapshot

--- scripts/test_shared.sh
+++ scripts/test_shared.sh
@@ -1,6 +1,15 @@
-for file in ./examples/fish/share/**/*.fish
+output=$(for file in ./examples/fish/share/**/*.fish
 do
     ./node_modules/.bin/tree-sitter parse $file | grep ERROR
-done
+done)
 
-exit 0
+snapshot=$(cat ./scripts/shared_output.txt)
+
+if test "$output" == "$snapshot"; then
+    echo OK
+else
+    echo Ouput does not match the snapshot
+    printf "output:\n$output"
+    printf "\n\n----------\n\n"
+    printf "snapshot:\n$snapshot"
+fi

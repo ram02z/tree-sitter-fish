05fccf64bb3fd68db72dc66596c7c75740f06736
26797767+krnik <26797767+krnik@users.noreply.github.com>
2021-07-06 20:34:05 +0200

    chore: update test_shared script

--- scripts/test_shared.sh
+++ scripts/test_shared.sh
@@ -4,14 +4,14 @@ code=0
 
 for file in ./examples/fish/share/**/*.fish
 do
-    errors=$(./node_modules/.bin/tree-sitter parse $file | grep ERROR)
+    errors=$(./node_modules/.bin/tree-sitter parse $file | grep -E "^\s*\(ERROR")
 
     if test "$errors" != ""; then
         snap_file=$(echo $file | sed -r -e "s/[\.\/_]+/_/g")
         snapshot="$(cat ./scripts/shared_out/$snap_file)"
 
         if test "$errors" != "$snapshot"; then
-            echo Ouput does not match the snapshot
+            echo "Ouput does not match the snapshot for $snap_file"
             printf "output:\n$errors"
             printf "\n\n----------\n\n"
             printf "snapshot:\n$snapshot\n"
